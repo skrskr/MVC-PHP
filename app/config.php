73519263ccd2787da0ba98b10b4568374eac03f1
skrskr <mohamed1.sakr1996@gmail.com>
2020-01-17 14:05:43 +0200

    adding database handler classes

--- app/config.php
+++ app/config.php
@@ -6,3 +6,12 @@ if(!defined('DS')) {
 
 define('APP_PATH', dirname(realpath(__FILE__)));
 define('VIEWS_PATH', APP_PATH . DS . 'views' . DS);
+
+
+/* Database */
+defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
+defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
+defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', 'rebo');
+defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'semsarak');
+defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
+defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
